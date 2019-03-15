3da97bf8eb67ce353761966c298001e4539b7724
cgsdfc <cgsdfc@126.com>
2019-03-15 11:18:26 +0800

    Python compatibility and license heading.

--- bleu_metric.py
+++ bleu_metric.py
@@ -1,3 +1,24 @@
+# Copyright 2019 Cong Feng. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Driver script to compute BLEU score."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import argparse
 import bleu
 
@@ -44,7 +65,7 @@ if __name__ == '__main__':
 
     references = _read_references(args.references, len(translations))
 
-    bleu_score, *_ = bleu.compute_bleu(
+    bleu_score, _, _, _ = bleu.compute_bleu(
         reference_corpus=references,
         translation_corpus=translations,
         max_order=args.ngrams,
